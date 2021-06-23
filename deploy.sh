36c37f22f795d7a451d6c14fa01dfb193fc1e499
mshivakumargit <45810308+mshivakumargit@users.noreply.github.com>
2021-06-23 08:25:28 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ docker pull mongo
 docker pull node
 git clone https://github.com/mshivakumargit/activitymicroservice.git
 docker build -t activitymicroservice .
+docker run -d -t --name mongodbserver mongo
+docker run -d -t -p 9090:9090 --name api --link mongoserver:mongoserver activitymicroservice
